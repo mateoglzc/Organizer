3d102345fcf7eb059a554a8b9573188c041ee566
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-10-20 23:09:48 -0500

    Added a few Notes 📝

--- Notes.py
+++ Notes.py
@@ -3,3 +3,13 @@
 # import system library
 import os
 
+print(f" The os name is: {os.name}")
+
+# Current Working Directory
+print(os.getcwd())
+
+# To print absolute path on your system 
+print(os.path.abspath()  
+  
+# To print files and directories in the current directory on your system
+print(os.listdir()) 
